c7c417df1c5c5eebb6b3d9095ef7ba926441fc95
Michael Enger <git@michaelenger.com>
2022-09-12 01:08:49 +0100

    Basic implementation of gameplay

--- src/game.rs
+++ src/game.rs
@@ -1,21 +1,30 @@
 use crate::input::{ButtonState, Gamepad};
 use crate::palette::set_colors;
 use crate::wasm4;
+use fastrand::Rng;
 
 pub struct Game {
     destiny: u32,
     potatoes: u32,
     orcs: u32,
+    main_result: u8,
+    sub_result: u8,
+    hurl_cost: u8,
     gamepad: Gamepad,
+    rng: Rng,
 }
 
 impl Game {
     pub fn new() -> Self {
         Game {
-            destiny: 1,
-            potatoes: 2,
-            orcs: 3,
+            destiny: 0,
+            potatoes: 0,
+            orcs: 0,
+            main_result: 0,
+            sub_result: 0,
+            hurl_cost: 1,
             gamepad: Gamepad::new(),
+            rng: Rng::with_seed(1),
         }
     }
 
@@ -41,23 +50,28 @@ impl Game {
         // Buttons
         if self.gamepad.one == ButtonState::Held || self.gamepad.one == ButtonState::Pressed {
             set_colors(0x04);
-            wasm4::text("(X)ROLL", 10, 141);
+            wasm4::text("(X)ROLL", 10, 131);
         } else {
             set_colors(0x02);
-            wasm4::text("(X)ROLL", 10, 141);
+            wasm4::text("(X)ROLL", 10, 131);
             set_colors(0x04);
-            wasm4::text("(X)ROLL", 10, 140);
+            wasm4::text("(X)ROLL", 10, 130);
         }
 
         if self.gamepad.two == ButtonState::Held || self.gamepad.two == ButtonState::Pressed {
             set_colors(0x04);
-            wasm4::text("(Z)HURL", 90, 141);
+            wasm4::text("(Z)HURL", 90, 131);
         } else {
             set_colors(0x02);
-            wasm4::text("(Z)HURL", 90, 141);
+            wasm4::text("(Z)HURL", 90, 131);
             set_colors(0x04);
-            wasm4::text("(Z)HURL", 90, 140);
+            wasm4::text("(Z)HURL", 90, 130);
         }
+
+        // Roll and hurl data
+        set_colors(0x04);
+        wasm4::text(format!("{} {}", self.main_result, self.sub_result), 28, 142);
+        wasm4::text(format!("{}", self.hurl_cost), 118, 142);
     }
 
     fn draw_pips(&self, num: u32, x: i32, y: i32) {
@@ -76,10 +90,88 @@ impl Game {
         }
     }
 
+    fn roll(&mut self) {
+        self.main_result = self.rng.u8(1..7);
+        self.sub_result = self.rng.u8(1..7);
+
+        if self.main_result < 3 {
+            // In the Garden...
+            match self.sub_result {
+                1 => {
+                    self.potatoes += 1;
+                }
+                2 => {
+                    self.potatoes += 1;
+                    self.destiny += 1;
+                }
+                3 => {
+                    self.destiny += 1;
+                    self.orcs += 1;
+                }
+                4 => {
+                    self.orcs += 1;
+                    if self.potatoes > 0 {
+                        self.potatoes -= 1;
+                    }
+                }
+                5 => {
+                    if self.potatoes > 0 {
+                        self.potatoes -= 1;
+                    }
+                }
+                6 => {
+                    self.potatoes += 2;
+                }
+                0 | 7..=u8::MAX => panic!("how?"),
+            }
+        } else if self.main_result < 5 {
+            // A Knock at the Door...
+            match self.sub_result {
+                1 => {
+                    self.orcs += 1;
+                }
+                2 => {
+                    self.destiny += 1;
+                }
+                3 => {
+                    self.orcs += 1;
+                    self.destiny += 1;
+                }
+                4 => {
+                    if self.potatoes > 0 {
+                        self.potatoes -= 1;
+                    }
+                    self.orcs += 2;
+                }
+                5 => {
+                    self.destiny += 1;
+                }
+                6 => {
+                    self.potatoes += 2;
+                }
+                0 | 7..=u8::MAX => panic!("how?"),
+            }
+        } else {
+            // The world becomes darker...
+            self.hurl_cost += 1
+        }
+    }
+
+    fn hurl(&mut self) {
+        if self.hurl_cost as u32 <= self.potatoes {
+            self.potatoes -= self.hurl_cost as u32;
+        }
+    }
+
     pub fn update(&mut self) {
         self.gamepad.update();
 
-        // other updates
+        if self.gamepad.one == ButtonState::Released {
+            self.roll();
+        }
+        if self.gamepad.two == ButtonState::Released {
+            self.hurl();
+        }
 
         self.draw();
     }
