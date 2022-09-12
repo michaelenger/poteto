f14a45d3b70a7ea5265e4815d8b256ddf5d0f2ec
Michael Enger <git@michaelenger.com>
2022-09-12 01:17:12 +0100

    Add game over state

--- src/game.rs
+++ src/game.rs
@@ -47,31 +47,38 @@ impl Game {
         self.draw_pips(self.potatoes, 5, 70);
         self.draw_pips(self.orcs, 5, 100);
 
-        // Buttons
-        if self.gamepad.one == ButtonState::Held || self.gamepad.one == ButtonState::Pressed {
+        if self.is_game_over() {
             set_colors(0x04);
-            wasm4::text("(X)ROLL", 10, 131);
+            wasm4::text("GAME OVER", 43, 131);
+            set_colors(0x03);
+            wasm4::text("GAME OVER", 42, 130);
         } else {
-            set_colors(0x02);
-            wasm4::text("(X)ROLL", 10, 131);
-            set_colors(0x04);
-            wasm4::text("(X)ROLL", 10, 130);
-        }
+            // Buttons
+            if self.gamepad.one == ButtonState::Held || self.gamepad.one == ButtonState::Pressed {
+                set_colors(0x04);
+                wasm4::text("(X)ROLL", 10, 131);
+            } else {
+                set_colors(0x02);
+                wasm4::text("(X)ROLL", 10, 131);
+                set_colors(0x04);
+                wasm4::text("(X)ROLL", 10, 130);
+            }
 
-        if self.gamepad.two == ButtonState::Held || self.gamepad.two == ButtonState::Pressed {
-            set_colors(0x04);
-            wasm4::text("(Z)HURL", 90, 131);
-        } else {
-            set_colors(0x02);
-            wasm4::text("(Z)HURL", 90, 131);
-            set_colors(0x04);
-            wasm4::text("(Z)HURL", 90, 130);
-        }
+            if self.gamepad.two == ButtonState::Held || self.gamepad.two == ButtonState::Pressed {
+                set_colors(0x04);
+                wasm4::text("(Z)HURL", 90, 131);
+            } else {
+                set_colors(0x02);
+                wasm4::text("(Z)HURL", 90, 131);
+                set_colors(0x04);
+                wasm4::text("(Z)HURL", 90, 130);
+            }
 
-        // Roll and hurl data
-        set_colors(0x04);
-        wasm4::text(format!("{} {}", self.main_result, self.sub_result), 28, 142);
-        wasm4::text(format!("{}", self.hurl_cost), 118, 142);
+            // Roll and hurl data
+            set_colors(0x04);
+            wasm4::text(format!("{} {}", self.main_result, self.sub_result), 28, 142);
+            wasm4::text(format!("{}", self.hurl_cost), 118, 142);
+        }
     }
 
     fn draw_pips(&self, num: u32, x: i32, y: i32) {
@@ -163,14 +170,20 @@ impl Game {
         }
     }
 
+    fn is_game_over(&self) -> bool {
+        self.destiny > 9 || self.potatoes > 9 || self.orcs > 9
+    }
+
     pub fn update(&mut self) {
         self.gamepad.update();
 
-        if self.gamepad.one == ButtonState::Released {
-            self.roll();
-        }
-        if self.gamepad.two == ButtonState::Released {
-            self.hurl();
+        if !self.is_game_over() {
+            if self.gamepad.one == ButtonState::Released {
+                self.roll();
+            }
+            if self.gamepad.two == ButtonState::Released {
+                self.hurl();
+            }
         }
 
         self.draw();
